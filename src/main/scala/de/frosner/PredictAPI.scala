9fe2fab0b6aae5e790bcde25af88b088c837a267
Frank Rosner <frank@fam-rosner.de>
2018-05-24 14:24:48 +0200

    dont exit

--- src/main/scala/de/frosner/PredictAPI.scala
+++ src/main/scala/de/frosner/PredictAPI.scala
@@ -53,9 +53,6 @@ object PredictAPI {
     val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
 
     println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-    bindingFuture
-      .flatMap(_.unbind()) // trigger unbinding from the port
-      .onComplete(_ => system.terminate()) // and shutdown when done
   }
 
   def invert(img: INDArray): INDArray = {
