efcb5274f5d026efa79c59c7e122b94cc768a783
Frank Rosner <frank@fam-rosner.de>
2018-05-24 21:21:55 +0200

    add 2 pairs of convolution and pooling layers

--- src/main/scala/de/frosner/TrainMain.scala
+++ src/main/scala/de/frosner/TrainMain.scala
@@ -18,10 +18,11 @@ package de.frosner
 
 import java.io.File
 
+import org.deeplearning4j.eval.Evaluation
 import org.deeplearning4j.nn.api.OptimizationAlgorithm
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration
 import org.deeplearning4j.nn.conf.inputs.InputType
-import org.deeplearning4j.nn.conf.layers.{DenseLayer, OutputLayer}
+import org.deeplearning4j.nn.conf.layers._
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork
 import org.deeplearning4j.nn.weights.WeightInit
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener
@@ -30,12 +31,6 @@ import org.nd4j.linalg.activations.Activation
 import org.nd4j.linalg.learning.config.Nesterovs
 import org.nd4j.linalg.lossfunctions.LossFunctions
 
-/**
- * Two-layer MLP for MNIST using DL4J-style NeuralNet
- * model construction pattern.
- *
- * @author David Kale
- */
 object TrainMain extends App {
 
   val hiddenSize = 512
@@ -44,7 +39,7 @@ object TrainMain extends App {
   val learningRate: Double = 0.0015
   val decay: Double = 0.005
   val scoreFrequency = 1000
-  val numEpochs = 1
+  val numEpochs = 3
 
   val DATA_PATH = "src/main/resources/mnist_png/"
 
@@ -52,40 +47,86 @@ object TrainMain extends App {
   val mnistTest = MnistLoader.fromDirectory(new File(DATA_PATH + "testing"))
 
   val conf = new NeuralNetConfiguration.Builder()
-    .seed(123)
+    .seed(seed)
+    .l2(0.0005)
+    .weightInit(WeightInit.XAVIER)
     .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
-    .updater(new Nesterovs(0.006, 0.9))
-    .l2(1e-4)
-    .list
+    .updater(new Nesterovs(0.01, 0.9))
+    .list()
     .layer(
       0,
-      new DenseLayer.Builder()
-        .nIn(MnistLoader.height * MnistLoader.width)
-        .nOut(100)
-        .activation(Activation.RELU)
-        .weightInit(WeightInit.XAVIER)
-        .build
+      new ConvolutionLayer.Builder(5, 5)
+        .nIn(MnistLoader.channels)
+        .stride(1, 1)
+        .nOut(20) // number of filters
+        .activation(Activation.IDENTITY)
+        .build()
     )
     .layer(
       1,
+      new SubsamplingLayer.Builder(PoolingType.MAX)
+        .kernelSize(2, 2)
+        .stride(2, 2)
+        .build()
+    )
+    .layer(
+      2,
+      new ConvolutionLayer.Builder(5, 5)
+      //Note that nIn need not be specified in later layers
+        .stride(1, 1)
+        .nOut(50)
+        .activation(Activation.IDENTITY)
+        .build()
+    )
+    .layer(
+      3,
+      new SubsamplingLayer.Builder(PoolingType.MAX)
+        .kernelSize(2, 2)
+        .stride(2, 2)
+        .build()
+    )
+    .layer(
+      4,
+      new DenseLayer.Builder()
+        .activation(Activation.RELU)
+        .nOut(500)
+        .build()
+    )
+    .layer(
+      5,
       new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
-        .nIn(100)
         .nOut(MnistLoader.nClasses)
         .activation(Activation.SOFTMAX)
-        .weightInit(WeightInit.XAVIER)
-        .build
+        .build()
     )
-    .pretrain(false)
-    .backprop(true)
     .setInputType(InputType.convolutional(MnistLoader.height, MnistLoader.width, MnistLoader.channels))
-    .build
+    .backprop(true)
+    .pretrain(false)
+    .build();
 
   val model = new MultiLayerNetwork(conf)
 
   model.setListeners(new ScoreIterationListener(10))
 
   // fit once (one epoch)
-  model.fit(mnistTrain);
+
+  for (i <- 1 to numEpochs) {
+    model.fit(mnistTrain)
+
+    // evaluate model
+    // Create Eval object with 10 possible classes
+    val eval = new Evaluation(MnistLoader.nClasses)
+
+    // Evaluate the network
+    while (mnistTest.hasNext) {
+      val next = mnistTest.next()
+      val output = model.output(next.getFeatureMatrix)
+      eval.eval(next.getLabels, output)
+    }
+
+    println(eval.stats)
+    mnistTest.reset()
+  }
 
   ModelSerializer.writeModel(model, "model.zip", false)
 
