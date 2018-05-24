c50589b74434e2716af699161adbb3e6c469b29d
Frank Rosner <frank@fam-rosner.de>
2018-05-24 10:13:39 +0200

    use only native dependencies of the current os and arch

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,11 +1,24 @@
 import sbt._
 
 object Dependencies {
-  private val deeplearning4jVersion = "1.0.0-alpha"
+  private lazy val deeplearning4jVersion = "1.0.0-alpha"
+  private lazy val os = {
+    val osName = sys.props("os.name").toLowerCase
+    if (osName.contains("linux"))
+      "linux"
+    else if (osName.contains("mac os x"))
+      "macosx"
+    else if (osName.contains("windows"))
+      "windows"
+    else
+      throw new IllegalArgumentException(s"Unknown OS name '$osName'")
+  }
+  private lazy val arch = sys.props("os.arch")
+  private lazy val platform = s"$os-$arch"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.1"
   lazy val deeplearning4j = List(
-    "org.nd4j" % "nd4j-native-platform" % deeplearning4jVersion,
     "org.deeplearning4j" % "deeplearning4j-nn" % deeplearning4jVersion,
+    "org.nd4j" % "nd4j-native-platform" % deeplearning4jVersion classifier platform,
     "org.datavec" % "datavec-api" % deeplearning4jVersion
   )
   lazy val akkaHttp = List(
