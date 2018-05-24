effc3c6abfae404498ebeea2f558b4b06fa3c544
Frank Rosner <frank@fam-rosner.de>
2018-05-24 14:18:34 +0200

    dont exit on RETURN

--- src/main/scala/de/frosner/PredictAPI.scala
+++ src/main/scala/de/frosner/PredictAPI.scala
@@ -53,7 +53,6 @@ object PredictAPI {
     val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
 
     println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-    StdIn.readLine() // let it run until user presses return
     bindingFuture
       .flatMap(_.unbind()) // trigger unbinding from the port
       .onComplete(_ => system.terminate()) // and shutdown when done
