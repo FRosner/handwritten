a98a70f21c533fe31d5e9362dce5b8c0e509a832
Frank Rosner <frank@fam-rosner.de>
2018-05-24 10:46:02 +0200

    use native and not platform

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -30,7 +30,7 @@ object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.1"
   lazy val deeplearning4j = List(
     "org.deeplearning4j" % "deeplearning4j-nn" % deeplearning4jVersion,
-    "org.nd4j" % "nd4j-native-platform" % deeplearning4jVersion classifier platform,
+    "org.nd4j" % "nd4j-native" % deeplearning4jVersion classifier platform,
     "org.datavec" % "datavec-api" % deeplearning4jVersion
   )
   lazy val akkaHttp = List(
