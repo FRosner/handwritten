2af16cf8ad1edb4a51dd7503e272280c7396da34
Frank Rosner <frank@fam-rosner.de>
2018-05-24 10:13:23 +0200

    use only neural network dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,8 +4,8 @@ object Dependencies {
   private val deeplearning4jVersion = "1.0.0-alpha"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.1"
   lazy val deeplearning4j = List(
-    "org.deeplearning4j" % "deeplearning4j-core" % deeplearning4jVersion,
     "org.nd4j" % "nd4j-native-platform" % deeplearning4jVersion,
+    "org.deeplearning4j" % "deeplearning4j-nn" % deeplearning4jVersion,
     "org.datavec" % "datavec-api" % deeplearning4jVersion
   )
   lazy val akkaHttp = List(
